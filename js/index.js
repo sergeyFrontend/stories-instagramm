41a15b609d79cb1fe3cb067741a38f2e6223b797
sergeyFrontend <utemishevsergey02@gmail.com>
2022-12-25 00:29:47 +0500

    add auto schange history

--- js/index.js
+++ js/index.js
@@ -1,32 +1,68 @@
 const chunkPrev = document.querySelector(".player-chunk-prev");
 const chunkNext = document.querySelector(".player-chunk-next");
 
-chunkPrev.addEventListener("click", () => {
-  function moveClass(className, activeClassName) {
-    let active = document.querySelector("." + activeClassName),
-      prev = active.previousElementSibling;
+// переключение назад
+const prev = () => {
+  moveClass("player-chunk-active", "previousElementSibling");
+  moveClass("timeline-chunk-active","previousElementSibling", (el) => {
+      const inner = el.querySelector(".timeline-chunk-inner"),
+            width = parseFloat(inner.style.width) || 0;
 
-    if (prev && prev.classList.contains(className)) {
-      active.classList.remove(activeClassName),
-        prev.classList.add(activeClassName);
+      el.querySelector(".timeline-chunk-inner").style.width = '';
+      return width <= 25;
     }
+  )};
+chunkPrev.addEventListener("click", prev);
+
+// переключение вперед
+const next = () => {
+  moveClass("player-chunk-active", "nextElementSibling");
+  const el = moveClass("timeline-chunk-active", "nextElementSibling");
+
+  if (el) {
+    el.querySelector(".timeline-chunk-inner").style.width = "";
+  }
+};
+chunkNext.addEventListener("click", next);
+
+// фунция которая принимает класс и метод , вперед или назад идет перелистывание
+function moveClass(className, method, pred) {
+  const active = document.querySelector("." + className),
+    next = active[method]; //active.method
+
+  if (pred && !pred(active)) {
+    return null;
   }
 
-  moveClass("timeline-chunk", "timeline-chunk-active");
-  moveClass("player-chunk", "player-chunk-active");
-});
+  if (next) {
+    active.classList.remove(className);
+    next.classList.add(className);
 
-chunkNext.addEventListener("click", function () {
-  function moveClass(className) {
-    let active = document.querySelector("." + className);
-
-    console.log(active.nextElementSibling);
-    if (active.nextElementSibling) {
-      active.classList.remove(className);
-      active.nextElementSibling.classList.add(className);
-    }
+    return active;
   }
+  return null;
+}
 
-  moveClass("timeline-chunk-active");
-  moveClass("player-chunk-active");
-});
+//ниже код отвечает за анимацию при автоматическом перелиставание
+
+let timer;
+function runInterval(time,step){
+  clearInterval(timer);
+
+ timer = setInterval(() => {
+    const active = document
+      .querySelector(".timeline-chunk-active")
+      .querySelector(".timeline-chunk-inner");
+
+    let width = parseFloat(active.style.width) || 0;
+
+    if (width === 100) {
+      next();
+      return;
+    }
+    active.style.width = String(width + step) + "%";
+  }, (time * 1000 * step) / 100);
+
+}
+
+runInterval(5,1)
